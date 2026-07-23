aa80bfc3bdaab84eb958d05224411b44c1f4f733
pradeep1328 <pradeepsunkari13@gmail.com>
2026-07-23 13:08:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function calculate() {
 
-    let income = document.getElementById("income").value;
-    let expense = document.getElementById("expense").value;
+    let income = Number(document.getElementById("income").value);
+    let expense = Number(document.getElementById("expense").value);
+
+    if (income === 0 && expense === 0) {
+        alert("Please enter Income and Expense.");
+        return;
+    }
 
     let balance = income - expense;
 
-    document.getElementById("balance").innerHTML = balance;
+    document.getElementById("balance").innerHTML = balance.toLocaleString("en-IN");
+
+    if (balance < 0) {
+        document.getElementById("balance").style.color = "red";
+    } else {
+        document.getElementById("balance").style.color = "green";
+    }
 }
